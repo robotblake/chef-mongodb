f8df6abcbf6154939d252124dbc40531495ec4fe
Blake Imsland <blake@retroco.de>
2013-08-08 08:37:44 -0700

    Require apt 1.8.2 or greater

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs and configures mongodb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apt'
+depends 'apt', '>= 1.8.2'
 
 supports 'ubuntu'
 supports 'debian'
